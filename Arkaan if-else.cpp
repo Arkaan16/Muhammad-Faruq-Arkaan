80b50a41a2b4e12111f2bcd75f95b30fb68f29c6
Muhammad Faruq Arkaan <112605831+Arkaan16@users.noreply.github.com>
2022-09-22 12:49:50 +0700

    Add files via upload

---
+++ Arkaan if-else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int nilai;
+	cin>>nilai;
+	if(nilai==1){
+		cout<<"satu\n";
+	}else if(nilai==2){
+		cout<<"dua\n";
+	}else if(nilai==3){
+		cout<<"tiga\n";	
+	}else if(nilai>=3){
+		cout<<"Bilangan lebih besar dari tiga\n";
+	}
+}
